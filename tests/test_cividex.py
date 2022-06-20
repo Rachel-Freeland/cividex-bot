702091f921225c28295dca8c1b0813406cf8871b
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-20 15:44:10 -0700

    added test ideas

---
+++ tests/test_cividex.py
@@ -0,0 +1,9 @@
+
+# Write Test to confirm pulling right data from DB
+
+# Write test to confirm proper formatting for tweet
+
+# Write test to check tweet length
+
+# Write test to check connection to twitter
+
