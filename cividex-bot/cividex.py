c2ba94844d1bfe8f036744145173fe46cf84e971
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-20 14:45:18 -0700

    added twitter connection

--- cividex-bot/cividex.py
+++ cividex-bot/cividex.py
@@ -1,5 +1,5 @@
 from os import environ as env
-
+from retrieve_tweet import fetch_tweet()
 import tweepy
 
 # Set Variables for .env
@@ -11,6 +11,9 @@ access_token_secret = env ['ACCESS_TOKEN_SECRET']
 
 #Twitter Authentication
 def authenticate():
+    '''
+    Authenticates bot with twitter server allowing for access to tweets. 
+    '''
     try: 
         auth = tweepy.OAuthHandler(
             consumer_key, consumer_secret, access_token, access_token_secret
@@ -19,5 +22,13 @@ def authenticate():
     except Exception as error:
         print(f'Unable to access and authenticate twitter API. Reason: {error}')
 
+# Main runtime
+
 def main():
     api = authenticate()
+    
+
+
+
+if __name__ == "__main__":
+    main()
